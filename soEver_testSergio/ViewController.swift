30587bc31b03ef8cabfbd89cb3f96a3f8fb7fe34
Ruben Dominguez <rubendominguez@iMac-de-Ruben.local>
2018-03-14 11:30:37 +0100

    Initial Commit

---
+++ soEver_testSergio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soEver_testSergio
+//
+//  Created by Ruben Dominguez on 14/3/18.
+//  Copyright © 2018 Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
